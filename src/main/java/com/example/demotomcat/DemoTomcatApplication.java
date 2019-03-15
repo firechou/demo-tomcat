73c88dc610a882afe67dcc434b4fcc2617ad5093
yan.zhou1 <yan.zhou1@china.zhaogang.com>
2019-03-15 14:35:50 +0800

    init project

---
+++ src/main/java/com/example/demotomcat/DemoTomcatApplication.java
@@ -0,0 +1,13 @@
+package com.example.demotomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTomcatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTomcatApplication.class, args);
+    }
+
+}
